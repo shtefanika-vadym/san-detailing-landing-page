e12baad4ef9abec031b952020ea1966435bc2457
Shtefanika Vadym <vadym.shtefanika.assist@natterbox.com>
2025-08-04 23:40:09 +0300

    feat: integrate next sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,8 +3,7 @@ module.exports = {
   siteUrl: 'https://www.sandetailing.ro/',
   generateRobotsTxt: true,
   changefreq: 'weekly',
-  priority: 0.7,
-  sitemapSize: 7000,
+  priority: 0.9,
   exclude: ['/404', '/500'],
 };
 
